315d6697ba79ec47c4cbefe446431fdd7f8d96c3
dattv <datthangvan@gmail.com>
2019-04-02 16:06:33 +0700

    update

--- helper/args.py
+++ helper/args.py
@@ -6,6 +6,20 @@
 import sys
 import numpy as np
 import tensorflow as tf
+from numpy.f2py.auxfuncs import F2PYError
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
+
+# Model network parameters
+flags.DEFINE_integer("scale", 2, "Scale factor for Super Resolution (should be 2 or more)")
+flags.DEFINE_integer("layers", 12, "Number of layers of feature xtraction CNNs")
+
+
+def get():
+    print("Python Interpreter version:%s" % sys.version[:3])
+    print("Tensorflow version:$s" % tf.__version__)
+    print("Numpy version:%s" % np.__version__)
+
+    # check which library you are using
+    return FLAGS
