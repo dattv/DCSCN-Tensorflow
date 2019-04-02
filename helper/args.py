4685c3a0b4975df8ec08003f4c715c7c341ff940
dattv <datthangvan@gmail.com>
2019-04-02 15:45:54 +0700

    update helper folder and args.py

---
+++ helper/args.py
@@ -0,0 +1,11 @@
+"""
+    Function for sharing arguments and their default values
+    Reference: https://github.com/jiny2001/dcscn-super-resolution/blob/master/helper/args.py
+"""
+
+import sys
+import numpy as np
+import tensorflow as tf
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
