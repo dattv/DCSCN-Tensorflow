1d7b71126da167d73d861115d4b0fb1579be97bd
dattv <datthangvan@gmail.com>
2019-04-02 17:03:42 +0700

    update

--- helper/utility.py
+++ helper/utility.py
@@ -14,3 +14,20 @@ import tensorflow as tf
 from PIL import Image
 from os.path import isfile, join
 from scipy import misc
+
+from scipy import misc
+
+
+class Timer:
+    def __init__(self, timer_count=100):
+        self.times = np.zeros(timer_count)
+        self.start_times = np.zeros(timer_count)
+        self.counts = np.zeros(timer_count)
+        self.timer_count = timer_count
+
+    def start(self, time_id):
+        self.start_times[time_id] = time.time()
+
+    def end(self, time_id):
+        self.times[time_id] += time.time() - self.start_times[time_id]
+        self.counts[time_id] += 1
