8b97f5af93761b1960b3351d1f16d097dc34959f
dat <datthangvan@gmail.com>
2019-04-03 22:12:12 +0700

    update loader

--- helper/loader.py
+++ helper/loader.py
@@ -16,3 +16,10 @@ INPUT_IMAGE_IDR = "input"
 INTERPOLATED_IMAGE_DIR = "interpolated"
 TRUE_IMAGE_DIR = "true"
 
+def build_image_set(file_path, chanels=1, scale=1, convert_ycbcr=True, resampleing_method="bicubic",
+                    print_console=True):
+    true_image = util.set_image_alignment(util.load_image(file_path, print_console=print_console), scale)
+
+    if chanels == 1 and true_image.shape[2] == 3 and convert_ycbcr:
+        true_image = util.convert_rgb_to_y(true_image)
+
